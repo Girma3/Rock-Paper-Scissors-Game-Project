db1cb91979ca770464d6f0cfa5b8b490b75e58b7
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-05-31 17:51:16 +0300

    functions created to make it five rounds game and some style added on css

--- main.js
+++ main.js
@@ -1,71 +1,143 @@
-function game() {//I use this function to play  rounds and track scores and to report at end.
+//dictionary to put user pick with same image
+const userchoices={
+    "Rock":"images/rock.jpg",
+    "Paper" : "images/paper.jpg",
+    "Scissors" : "images/scissors.jpg"
+}
+let computerSelection;
+let playerSelection;
+let results;
+var userScore = 0;
+var computerScore=0;
 
+// get the choice by click event on images 
+const userPicks=(hand)=>{
+    
+    const choices = document.querySelector(".choicebuttons")
+    
+        //after user click hide the buttons 
+       
+        choices.style.display="none"
 
+        //after the user clicks show the results
+        const result = document.querySelector(".showresults")
+         
+        result.style.display='flex'
 
+        //after the user picks hand show what picks
+       
 
-    function getComputerChoice() {
-        let choice = ["rock", "paper", "scissors"];
+    document.getElementById('userpicksimg').src=userchoices[hand];
+    
+   playerSelection=hand;
+   computerSelection=getComputerChoice();
 
-        //get random index from choice array
-        let randomIndex = Math.floor(Math.random() * choice.length)
+  //DOM method to show result and  score based on player and computer choice
+    const playerChoice = document.getElementById('playerchoose');
+         playerChoice.textContent=`you picked ${playerSelection}`;
 
-        //use random index to pick item from the array
-        let item = choice[randomIndex];
-        return item;
+    const compChoice = document.getElementById("compchoose"); 
+           compChoice.textContent=`computer picked ${computerSelection}`; 
+
+    const allResult = document.getElementById('currentresult');
+           allResult.textContent=playRound(playerSelection,computerSelection);
+        
+    const playerScore = document.getElementById("playerscore");
+       playerScore.textContent=`${userScore}`;
+
+    const compScore = document.getElementById("computerscore");
+       compScore.textContent=`${computerScore}`;
+  
+   }
+
+    const getComputerChoice=()=> {
+            let choice = ["Rock", "Paper", "Scissors"];
+
+            //get random index from choice array
+            let randomIndex =choice[Math.floor(Math.random() * 3)];
+
+            //use random index to pick item from the array
+            //let item = choice[randomIndex];
+            //pick the image for computer
+        
+            document.getElementById("computerpickimg").src=userchoices[randomIndex];
+        
+            return randomIndex
     }
 
-    let computerSelection = getComputerChoice();
-    
-    var playerSelection = "";//we letter use this inside playRound function to recive input repetively.
-
-    var userScore = 0;// we only add 1 when user win if its draw nothing added.
-
+   
+   
+   
+   
     function playRound(playerSelection, computerSelection) {
 
-        playerSelection = prompt("choose Rock, paper, or Scissors: ").toLocaleLowerCase();
+        
 
         if (playerSelection === computerSelection) {
             result = "It's a Draw.";
         }
 
-        else if (playerSelection === "rock" && computerSelection === "paper") {
+        else if (playerSelection === "Rock" && computerSelection === "Paper") {
+           
             result = "You Lose!,Paper beats Rock.";
+            computerScore++;
             
         }
-        else if (playerSelection === "rock" && computerSelection === "scissors") {
+        else if (playerSelection === "Rock" && computerSelection === "Scissors") {
             
             result = "You Win!,Rock beats Scissors.";
             userScore++;
         }
-        else if (playerSelection === "paper" && computerSelection === "rock") {
+        else if (playerSelection === "Paper" && computerSelection === "Rock") {
 
             result = "You Win!,Paper beats Rock.";
             userScore++;
 
         }
-        else if (playerSelection === "paper" && computerSelection === "scissors") {
+        else if (playerSelection === "Paper" && computerSelection === "Scissors") {
             result = "You Lose!,Scissors beats Paper.";
+            computerScore++;
         }
-        else if (playerSelection == "scissors" && computerSelection === "rock") {
+        else if (playerSelection == "Scissors" && computerSelection === "Rock") {
             result = "You Lose!,Rock beats scissors.";
+            computerScore++;
         }
-        else if (playerSelection === "scissors" && computerSelection === "paper") {
+        else if (playerSelection === "Scissors" && computerSelection === "Paper") {
 
             result = "You Win!,Scissors beats paper.";
             userScore++;
         }
-        else {
-            result = " Please, enter proper word again!";
-        }
-        console.log("you choose " + playerSelection);
-        console.log("computer choose " + computerSelection);
+        
+       
         return result;
     }
-console.log("On Round 1 " + playRound(playerSelection, computerSelection));
-console.log("On Round 2 " + playRound(playerSelection, computerSelection));
-console.log("On Round 3 " + playRound(playerSelection, computerSelection));
-console.log(`Your total score is ${userScore}.`);
+
+
+const playAgain=()=>{
+    
+    const choices = document.querySelector(".choicebuttons")
+    
+        //after user click hide the buttons 
+       console.log(choices)
+        choices.style.display="flex"
+
+        //after the user clicks show the results
+        const result = document.querySelector(".showresults");
+         
+        result.style.display='none'
+
+       if (userScore>=5){
+        
+        window.alert("Congratulations! You Won 5 rounds!");
+        location.reload();
+       } 
+       else if(computerScore>=5){
+        
+        window.alert("you lost the first  5 rounds try again");
+        location.reload();
+       }
+
 }
 
-game(); //we call this function to play 3 rounds
+
 
